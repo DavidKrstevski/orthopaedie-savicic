ec30bee090b3a988e4fb7b8f335110ebf37344ef
David Krstevski <DavidKrstevski10@gmail.com>
2022-11-27 22:17:37 +0100

    Commit Website v1.0

---
+++ main.js
@@ -0,0 +1,41 @@
+const navToggle = document.querySelector('.mobile-nav-toggle');
+const primaryNav = document.querySelector('.primary-navigation');
+
+navToggle.addEventListener('click', () => {
+  primaryNav.hasAttribute('data-visible') ? navToggle.setAttribute('aria-expanded', false) : navToggle.setAttribute('aria-expanded', true);
+  primaryNav.toggleAttribute('data-visible')
+});
+
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  slidesPerView: 1,
+  autoplay:{
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  loop: true,
+
+  breakpoints: {
+    '@1.5': {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    '@0.8': {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
